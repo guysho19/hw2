3787036c497c9b585fac0806e2bde2ab13570185
guysho19 <guy.shoef@campus.technion.ac.il>
2022-05-18 19:31:02 +0300

    implemented equals not completely

--- src/AccurateClock.java
+++ src/AccurateClock.java
@@ -5,6 +5,14 @@ public class AccurateClock extends Clock{
         super(hour, minute);
         this.seconds = seconds;
     }
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof AccurateClock)){
+            return false;
+        }
+        AccurateClock otherClock = (AccurateClock) other;
+        return this.hour == otherClock.hour && this.minute == otherClock.minute && otherClock.seconds==this.seconds;
+    }
 
     public int getSeconds() {
         return seconds;
--- src/Clock.java
+++ src/Clock.java
@@ -1,12 +1,19 @@
 public class Clock {
-    private int hour;
-    private int minute;
+    protected int hour;
+    protected int minute;
 
     public Clock(int hour, int minute) {
         this.hour = hour;
         this.minute = minute;
     }
-
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Clock)){
+            return false;
+        }
+        Clock otherClock = (Clock) other;
+        return this.hour == otherClock.hour && this.minute == otherClock.minute;
+    }
     public int getHour() { return hour; }
 
     public void setHour(int hour) { this.hour = hour; }
