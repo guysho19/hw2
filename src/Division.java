ce8c3a9a76b7797c7f38074e49da2a023d16113a
guysho19 <guy.shoef@campus.technion.ac.il>
2022-05-19 00:31:32 +0300

    IntegerLiteral + DoubleLiteral

---
+++ src/Division.java
@@ -0,0 +1,19 @@
+public class Division extends Expression {
+    private int valueInt;
+    private double valueDouble;
+
+    public Division(int valueInt, double valueDouble) {
+        this.valueInt = valueInt;
+        this.valueDouble = valueDouble;
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+    @Override
+    public double evaluate(){
+
+    }
+}
